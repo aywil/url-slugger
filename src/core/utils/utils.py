32454cb51d1b78247bbec80a44397124e946ccd5
aywil <imaybedeadz@gmail.com>
2025-12-22 19:43:36 +0700

    create validate_url dependencies

--- src/core/utils/utils.py
+++ src/core/utils/utils.py
@@ -11,3 +11,12 @@ def create_slug() -> str:
     for _ in range(6):
         slug += secrets.choice(GENERATE_SHORTLINK)
     return slug
+
+
+def validated_url(url: str) -> str:
+    if not validators.url(url):
+        if validators.url(f"https://{url}"):
+            url = f"https://{url}"
+        else:
+            raise ValueError("Invalid URL")
+    return url

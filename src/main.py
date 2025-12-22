34d097be06c996aa2e65f97b12d45592bd02deae
aywil <imaybedeadz@gmail.com>
2025-12-22 19:50:31 +0700

    create redirect to slug route

--- src/main.py
+++ src/main.py
@@ -2,10 +2,11 @@ from contextlib import asynccontextmanager
 
 import uvicorn
 from fastapi import Depends, FastAPI, HTTPException, Query, status
+from fastapi.responses import RedirectResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core import settings
-from core.db.crud import add_slug_db
+from core.db.crud import add_slug_db, url_from_slug
 from core.db.db import engine, get_db
 from core.utils import create_slug, validated_url
 
@@ -40,6 +41,27 @@ async def create_slug_url(
     )
 
     return slug
+
+
+@app.get("/{slug}")
+async def redirect_to_slug(
+    slug: str,
+    session: AsyncSession = Depends(get_db),
+) -> RedirectResponse:
+    url = await url_from_slug(
+        slug=slug,
+        session=session,
+    )
+    if not url:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Slug doesn't exist",
+        )
+
+    return RedirectResponse(
+        url=url,
+        status_code=302,
+    )
 
 
 if __name__ == "__main__":

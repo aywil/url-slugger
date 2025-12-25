8d1d678586ec2883fa0f99af1fabb2d84d9bb6ff
aywil <imaybedeadz@gmail.com>
2025-12-25 19:50:51 +0700

    change crud logic

--- src/core/db/crud.py
+++ src/core/db/crud.py
@@ -3,28 +3,48 @@ import secrets
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import ShortLink
-from core.schemas import UrlInfo
+from core.schemas import SlugInfo
 
-from .exceptions import NotFoundSlugError, NotFoundStatisticsError
-from .utils import GENERATE_SHORTLINK, get_shortlink_table_from_slug, validated_url
+from .exceptions import CustomSlugError, NotFoundSlugError, NotFoundStatisticsError
+from .utils import (
+    GENERATE_SHORTLINK,
+    get_shortlink_table_from_slug,
+)
 
 
 async def create_slug(
     url: str,
     session: AsyncSession,
 ) -> str:
-    try:
-        safe_url = validated_url(url=url)
-    except ValueError as e:
-        raise e
-
     slug = ""
     for _ in range(6):
         slug += secrets.choice(GENERATE_SHORTLINK)
 
     shortlink = ShortLink(
         slug=slug,
-        url=safe_url,
+        url=str(url),
+    )
+    session.add(shortlink)
+    await session.commit()
+
+    return slug
+
+
+async def create_custom_slug(
+    url: str,
+    slug: str,
+    session: AsyncSession,
+) -> str:
+    result = await get_shortlink_table_from_slug(
+        slug=str(slug),
+        session=session,
+    )
+    if result:
+        raise CustomSlugError(f"Slug: {slug} is busy")
+
+    shortlink = ShortLink(
+        slug=str(slug),
+        url=str(url),
     )
     session.add(shortlink)
     await session.commit()
@@ -43,13 +63,15 @@ async def get_url_from_slug(
     )
     if not result:
         raise NotFoundSlugError(f"Slug {slug} doesn't exist")
+    result.clicks += 1
+    await session.commit()
     return result.url
 
 
 async def get_stats_from_slug(
     slug: str,
     session: AsyncSession,
-) -> UrlInfo | None:
+) -> SlugInfo | None:
     stats = await get_shortlink_table_from_slug(
         slug=slug,
         session=session,
@@ -57,7 +79,3 @@ async def get_stats_from_slug(
     if not stats:
         raise NotFoundStatisticsError(f"Statistics about {slug} doesn't exist")
     return stats
-
-
-async def count_clicks_to_slug():
-    pass
